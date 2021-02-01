e341c6445eae8e0438d9ef26b4a076e22c7d6f22
Hakan Girgin <hakangirgin7@gmail.com>
2021-02-01 16:16:07 +0000

    30.01.2021

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstrack;
+using DataAccess.Abstrack;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları buraya yazılır.
+            return _productDal.GetAll(); 
+        }
+    }
+}
