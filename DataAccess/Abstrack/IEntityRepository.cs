08b7195721e341ba59a3dab82d3b89fc5f8973b1
Hakan Girgin <hakangirgin7@gmail.com>
2021-02-03 20:53:01 +0000

    03.02.2021

---
+++ DataAccess/Abstrack/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstrack;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstrack
+{
+    //class referans tip demek
+    //IEntity IEntity olabilir yada ondan implement edilen bir class olabilir.
+    //new demek ise newlenebilir olamalıdır. Ancak IEntity interface olduğu ve newlenemediği için istediğimiz gibi olur.
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter =null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
