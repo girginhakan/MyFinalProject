e2e11af2dd1ee1106b8510b35fab4913e9c27a4e
Hakan Girgin <hakangirgin7@gmail.com>
2021-02-14 00:12:57 +0000

    13.02.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstrack;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]//Attribute:bir class ile ilgili bilgi verme, imzalama gibi
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled
+        //naming convention
+        //IoC Container
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger
+            //Depencency chain 
+            var result = _productService.GetAll();
+            if (result.Success==true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        
+    }
+}
